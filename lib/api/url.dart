e8a9573549aef4c2148e5199c09bfa9ed88c8dc7
dhavalDev123 <dhaval.a@polynomial.ai>
2025-02-12 19:36:28 +0530

    minor changes

--- lib/api/url.dart
+++ lib/api/url.dart
@@ -1,10 +1,5 @@
 class Url {
-  // TODO:base url
-
   final String baseUrl = "https://api.dev.ostrumtech.com/ref-code/";
-
-  // TODO:end point
-
   final String airportDeployed = "airport-deployed";
   final String getAirportListByEmailDomain = "getAirportListByEmailDomain";
 }
